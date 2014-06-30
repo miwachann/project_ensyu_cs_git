c4a5b06b55ff15943124d2442a33cf51770077a2
frederica07 <frederica07@frederica.local>
2014-06-30 13:20:00 +0900

    第５回

---
+++ プロジェクト演習G13/第５回/peg_sol/ControllerPeg.java
@@ -0,0 +1,58 @@
+
+package peg_sol;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ControllerPeg
+{
+	
+	ViewPeg view;
+	ModelPeg model;
+	
+	public ControllerPeg(ViewPeg v, ModelPeg m)
+	{
+		view = v;
+        model = m;
+	}
+	
+	
+	/**
+	 * 新しいゲーム（ステージの選択）をするメソッド
+	 */
+	public void newGame()
+	{
+        //model.selectStage();
+		//gameflag = true;
+		//panel.repaint();
+	}
+	
+	
+	/**
+	 * ステージのやり直しをするメソッド
+	 */
+	public void resetStage()
+	{
+        //model.resetStage();
+		//panel.repaint();
+	}
+	
+	
+	/**
+	 * ステージを作成するモードに移行するメソッド
+	 */
+	public void makeStage()
+	{
+		//panel.repaint();
+	}
+	
+	
+	/**
+	 * クリックされた際の処理を行うメソッド
+	 */
+	public void didClick(int x, int y)
+	{
+		
+		
+	}
+}
---
+++ プロジェクト演習G13/第５回/peg_sol/ViewPeg.java
@@ -0,0 +1,65 @@
+
+package peg_sol;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.net.URL;
+import java.util.*;
+import javax.swing.*;
+
+
+public class ViewPeg extends JPanel
+{
+	int width, height;
+	//Modelとcontrollerのインスタンスを作成
+	ModelPeg model;
+	ControllerPeg controller;
+	static Image peg, ground, setPeg;
+    static int gridWidth, gridHeight;
+    
+    
+	/**
+	 * マスのサイズの設定
+	 */
+	private static void setGridSize(int w, int h)
+	{
+		gridWidth = w;
+        gridHeight = h;
+	}
+	
+	/**
+	 * ゲーム画面に使用する画像を取得
+	 */
+	private void getImages()
+	{
+		
+	}
+	
+	
+	public ViewPeg(int m, int n, ModelPeg model)
+	{
+		width = m;
+		height = n;
+        ModelPeg = model;
+	}
+	
+	
+	/**
+	 * MVCのContorollerのインスタンスを作成
+	 * マウスイベントの処置
+	 */
+	public void setController(ControllerPeg ctr)
+	{
+		controller = ctr;
+	}
+	
+	
+	
+	/**
+	 * ウィンドウ内の描画の処理を行う
+	 */
+	public void paintComponent(Graphics g)
+	{
+		
+	}
+}
